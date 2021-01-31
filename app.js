3c2ccdb0049eeb71c58ec860b250cd03913b85f9
chirag-aa <chiragambwani259@gmail.com>
2021-01-31 21:42:19 +0530

    Code: Update
    Added 3 files to the repo

---
+++ app.js
@@ -0,0 +1,8 @@
+var translateBtn=document.querySelector("#translate-btn");
+var textinp=document.querySelector("#inputText");
+var inputtext=document.querySelector("#input");
+translateBtn.addEventListener("click",function buttonclick(){
+  //  console.log("Clicked!");
+  //  console.log("input",textinp.value);
+    inputtext.innerText="ajajjajjajjaja  "+textinp.value;
+})
