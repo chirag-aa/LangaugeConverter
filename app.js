9976e66c6a17746fe341406250921ae45ebb267e
chirag-aa <chiragambwani259@gmail.com>
2021-03-11 17:00:12 +0530

    complete:project

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var translateBtn=document.querySelector("#translate-btn");
 var textinp=document.querySelector("#inputText");
 var inputtext=document.querySelector("#input");
+
+var serverurl= "https://api.funtranslations.com/translate/minion.json";
+
+function gettranslationurl(text){
+  return serverurl+"?"+"text="+text;
+}
+
+function errorhandler(error){
+  console.log("error accured",error);
+  alert("someting went wrong try after some time");
+}
+
 translateBtn.addEventListener("click",function buttonclick(){
   //  console.log("Clicked!");
   //  console.log("input",textinp.value);
-    inputtext.innerText="ajajjajjajjaja  "+textinp.value;
+    // inputtext.innerText="ajajjajjajjaja  "+textinp.value;
+    var enteredtext=textinp.value;
+    fetch(gettranslationurl(enteredtext))
+    .then(Response=>Response.json())
+    .then(json=>{
+          var translatedtext=json.contents.translated;
+          inputtext.innerText=translatedtext;
+    })
+    .catch(errorhandler);
 })
